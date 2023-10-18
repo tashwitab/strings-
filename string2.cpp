5286d0f6fc9d84617b31d6d216603c97baf5c550
Tashwita Bhirud <142425346+tashwitab@users.noreply.github.com>
2023-10-18 09:04:24 +0530

    Update string2.cpp

---
+++ string2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char str[20];
+    cout<<"Enter Your full name:";
+    cin.get(str,20);
+    cout<<"Your name is:"<<str;
+    cout<<endl;
+    return 0;
+
+}
+     
